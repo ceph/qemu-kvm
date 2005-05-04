f9e746135dc2600db1fbf97cd91374c9fbb49a5d
sshwarts <sshwarts>
2005-05-04 16:05:05 +0000

    Update MP tables comments in rombios

--- kvm/bios/rombios.c
+++ kvm/bios/rombios.c
@@ -10375,7 +10375,7 @@ mp_config_proc0:
   db 0 ;; entry type=processor
   db 0 ;; local APIC id
   db 0x11 ;; local APIC version number
-  db 3 ;; cpu flags: enabled
+  db 3 ;; cpu flags: enabled, bootstrap processor
   db 0,6,0,0 ;; cpu signature
   dw 0x201,0 ;; feature flags
   dw 0,0 ;; reserved
@@ -10537,7 +10537,7 @@ mp_config_proc0:
   db 0 ;; entry type=processor
   db 0 ;; local APIC id
   db 0x11 ;; local APIC version number
-  db 3 ;; cpu flags: bootstrap cpu
+  db 3 ;; cpu flags: enabled, bootstrap processor
   db 0,6,0,0 ;; cpu signature
   dw 0x201,0 ;; feature flags
   dw 0,0 ;; reserved
