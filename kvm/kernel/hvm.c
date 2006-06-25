b98536ed026cc5dd901e426b1abc1296197191fc
Avi Kivity <avi@qumranet.com>
2006-06-25 13:30:59 +0000

    kvm: refuse to load the module if hvm (VT) not present
    VT detection not coded yet.

--- kvm/kernel/hvm.c
+++ kvm/kernel/hvm.c
@@ -1,1 +1,27 @@
 /* hardware virtual machine support module */
+
+#include <linux/module.h>
+#include <linux/errno.h>
+
+static int cpu_has_hvm_support(void)
+{
+	return 0;
+}
+
+static __init int hvm_init(void)
+{
+	if (!cpu_has_hvm_support()) {
+		printk(KERN_ERR "hvm: no hardware support\n");
+		return -EOPNOTSUPP;
+	}
+	/* VMXON */
+	return 0;
+}
+
+static __exit void hvm_exit(void)
+{
+	/* VMXOFF */
+}
+
+module_init(hvm_init)
+module_exit(hvm_exit)
