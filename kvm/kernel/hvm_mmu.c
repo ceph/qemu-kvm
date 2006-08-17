2900f215f5ab74bf6dc363e51fe151a4f98d7628
Yaniv Kamay <yaniv@qumranet.com>
2006-08-17 14:55:24 +0000

    kvm: fix hugetlb translation in paging64_fetch_pte64()

--- kvm/kernel/hvm_mmu.c
+++ kvm/kernel/hvm_mmu.c
@@ -681,7 +681,14 @@ static u64 paging64_fetch_pte64(struct hvm_vcpu *vcpu, unsigned long vaddr)
 
 	init_walker(&walker, vcpu);
 	guest_pte = *fetch_guest64(vcpu, &walker, PT64_PAGE_TABLE_LEVEL, vaddr);
-	if (guest_pte & PT64_PAGE_SIZE_MASK) {
+
+	if (!is_present_pte64(guest_pte)) {
+		printk("%s: 0x%lx not present", __FUNCTION__, vaddr);
+	}
+
+	if (is_present_pte64(guest_pte) &&
+	    walker.level == PT64_DIRECTORY_LEVEL) {
+		ASSERT(guest_pte & PT64_PAGE_SIZE_MASK);
 		guest_pte |= vaddr & 0x1ff000;
 		guest_pte &= ~PT64_PAGE_SIZE_MASK;
 	}
