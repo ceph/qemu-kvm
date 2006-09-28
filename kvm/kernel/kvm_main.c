8a8aa88a9affeec1bd27553c1cc234cdcbd76f4a
Avi Kivity <avi@qumranet.com>
2006-09-28 08:05:29 +0000

    kvm: improve register loading from inline assembly
    instead of magic offsets (which caused a bug already), let the compiler
    figure out the offsets.

--- kvm/kernel/kvm_main.c
+++ kvm/kernel/kvm_main.c
@@ -2260,43 +2260,43 @@ again:
 	      "pop  %%rbp; pop  %%rdi; pop  %%rsi;"	       \
 	      "pop  %%rdx; pop  %%rbx; pop  %%rax"
 #define LOAD_GUEST_REGS \
-	"mov 128(%3), %%rax \n\t" \
+	"mov %c[cr2](%3), %%rax \n\t" \
 	"mov %%rax, %%cr2 \n\t"   \
-	"mov  0(%3),  %%rax \n\t" \
-	"mov  24(%3), %%rbx \n\t" \
-	"mov  16(%3), %%rdx \n\t" \
-	"mov  48(%3), %%rsi \n\t" \
-	"mov  56(%3), %%rdi \n\t" \
-	"mov  40(%3), %%rbp \n\t" \
-	"mov  64(%3), %%r8  \n\t" \
-	"mov  72(%3), %%r9  \n\t" \
-	"mov  80(%3), %%r10 \n\t" \
-	"mov  88(%3), %%r11 \n\t" \
-	"mov  96(%3), %%r12 \n\t" \
-	"mov 104(%3), %%r13 \n\t" \
-	"mov 112(%3), %%r14 \n\t" \
-	"mov 120(%3), %%r15 \n\t" \
-	"mov 8(%3),  %%rcx \n\t" /* kills %3 (rcx) */
+	"mov %c[rax](%3), %%rax \n\t" \
+	"mov %c[rbx](%3), %%rbx \n\t" \
+	"mov %c[rdx](%3), %%rdx \n\t" \
+	"mov %c[rsi](%3), %%rsi \n\t" \
+	"mov %c[rdi](%3), %%rdi \n\t" \
+	"mov %c[rbp](%3), %%rbp \n\t" \
+	"mov %c[r8](%3),  %%r8  \n\t" \
+	"mov %c[r9](%3),  %%r9  \n\t" \
+	"mov %c[r10](%3), %%r10 \n\t" \
+	"mov %c[r11](%3), %%r11 \n\t" \
+	"mov %c[r12](%3), %%r12 \n\t" \
+	"mov %c[r13](%3), %%r13 \n\t" \
+	"mov %c[r14](%3), %%r14 \n\t" \
+	"mov %c[r15](%3), %%r15 \n\t" \
+	"mov %c[rcx](%3), %%rcx \n\t" /* kills %3 (rcx) */
 
 #define STORE_GUEST_REGS \
 	"xchg %3,     0(%%rsp) \n\t" \
-	"mov %%rax,   0(%3) \n\t" \
-	"mov %%rbx,  24(%3) \n\t" \
-	"pushq 0(%%rsp); popq 8(%3) \n\t" \
-	"mov %%rdx,  16(%3) \n\t" \
-	"mov %%rsi,  48(%3) \n\t" \
-	"mov %%rdi,  56(%3) \n\t" \
-	"mov %%rbp,  40(%3) \n\t" \
-	"mov %%r8,   64(%3) \n\t" \
-	"mov %%r9,   72(%3) \n\t" \
-	"mov %%r10,  80(%3) \n\t" \
-	"mov %%r11,  88(%3) \n\t" \
-	"mov %%r12,  96(%3) \n\t" \
-	"mov %%r13, 104(%3) \n\t" \
-	"mov %%r14, 112(%3) \n\t" \
-	"mov %%r15, 120(%3) \n\t" \
+	"mov %%rax, %c[rax](%3) \n\t" \
+	"mov %%rbx, %c[rbx](%3) \n\t" \
+	"pushq 0(%%rsp); popq %c[rcx](%3) \n\t" \
+	"mov %%rdx, %c[rdx](%3) \n\t" \
+	"mov %%rsi, %c[rsi](%3) \n\t" \
+	"mov %%rdi, %c[rdi](%3) \n\t" \
+	"mov %%rbp, %c[rbp](%3) \n\t" \
+	"mov %%r8,  %c[r8](%3) \n\t" \
+	"mov %%r9,  %c[r9](%3) \n\t" \
+	"mov %%r10, %c[r10](%3) \n\t" \
+	"mov %%r11, %c[r11](%3) \n\t" \
+	"mov %%r12, %c[r12](%3) \n\t" \
+	"mov %%r13, %c[r13](%3) \n\t" \
+	"mov %%r14, %c[r14](%3) \n\t" \
+	"mov %%r15, %c[r15](%3) \n\t" \
 	"mov %%cr2, %%rax   \n\t" \
-	"mov %%rax, 128(%3) \n\t" \
+	"mov %%rax, %c[cr2](%3) \n\t" \
 	"mov 0(%%rsp), %3 \n\t"
 #else
 #define SP "esp"
@@ -2304,27 +2304,27 @@ again:
 #define POPA  "pop %%ecx; popa"
 
 #define LOAD_GUEST_REGS \
-	"mov 64(%3),  %%eax \n\t" \
+	"mov %c[cr2](%3), %%eax \n\t" \
 	"mov %%eax,   %%cr2 \n\t" \
-	"mov 0(%3),   %%eax \n\t" \
-	"mov 12(%3),  %%ebx \n\t" \
-	"mov 8(%3),   %%edx \n\t" \
-	"mov 24(%3),  %%esi \n\t" \
-	"mov 28(%3),  %%edi \n\t" \
-	"mov 20(%3),  %%ebp \n\t" \
-	"mov 4(%3),   %%ecx \n\t" /* kills %3 (ecx) */
+	"mov %c[rax](%3), %%eax \n\t" \
+	"mov %c[rbx](%3), %%ebx \n\t" \
+	"mov %c[rdx](%3), %%edx \n\t" \
+	"mov %c[rsi](%3), %%esi \n\t" \
+	"mov %c[rdi](%3), %%edi \n\t" \
+	"mov %c[rbp](%3), %%ebp \n\t" \
+	"mov %c[rcx](%3), %%ecx \n\t" /* kills %3 (ecx) */
 
 #define STORE_GUEST_REGS \
 	"xchg %3, 0(%%esp) \n\t" \
-	"mov %%eax,  0(%3) \n\t" \
-	"mov %%ebx, 12(%3) \n\t" \
-	"pushl 0(%%esp); popl 4(%3) \n\t" \
-	"mov %%edx,  8(%3) \n\t" \
-	"mov %%esi, 24(%3) \n\t" \
-	"mov %%edi, 28(%3) \n\t" \
-	"mov %%ebp, 20(%3) \n\t" \
+	"mov %%eax, %c[rax](%3) \n\t" \
+	"mov %%ebx, %c[rbx](%3) \n\t" \
+	"pushl 0(%%esp); popl %c[rcx](%3) \n\t" \
+	"mov %%edx, %c[rdx](%3) \n\t" \
+	"mov %%esi, %c[rsi](%3) \n\t" \
+	"mov %%edi, %c[rdi](%3) \n\t" \
+	"mov %%ebp, %c[rbp](%3) \n\t" \
 	"mov %%cr2, %%eax  \n\t" \
-	"mov %%eax, 64(%3) \n\t" \
+	"mov %%eax, %c[cr2](%3) \n\t" \
 	"mov 0(%%esp), %3"
 
 #endif
@@ -2351,7 +2351,25 @@ again:
 	      "done:"
 	      : "=g" (fail)
 	      : "r"(vcpu->launched), "r"((unsigned long)HOST_RSP),
-		"c"(vcpu->regs)
+		"c"(vcpu),
+		[rax]"i"(offsetof(struct kvm_vcpu, regs[VCPU_REGS_RAX])),
+		[rbx]"i"(offsetof(struct kvm_vcpu, regs[VCPU_REGS_RBX])),
+		[rcx]"i"(offsetof(struct kvm_vcpu, regs[VCPU_REGS_RCX])),
+		[rdx]"i"(offsetof(struct kvm_vcpu, regs[VCPU_REGS_RDX])),
+		[rsi]"i"(offsetof(struct kvm_vcpu, regs[VCPU_REGS_RSI])),
+		[rdi]"i"(offsetof(struct kvm_vcpu, regs[VCPU_REGS_RDI])),
+		[rbp]"i"(offsetof(struct kvm_vcpu, regs[VCPU_REGS_RBP])),
+#ifdef __x86_64__
+		[r8 ]"i"(offsetof(struct kvm_vcpu, regs[VCPU_REGS_R8 ])),
+		[r9 ]"i"(offsetof(struct kvm_vcpu, regs[VCPU_REGS_R9 ])),
+		[r10]"i"(offsetof(struct kvm_vcpu, regs[VCPU_REGS_R10])),
+		[r11]"i"(offsetof(struct kvm_vcpu, regs[VCPU_REGS_R11])),
+		[r12]"i"(offsetof(struct kvm_vcpu, regs[VCPU_REGS_R12])),
+		[r13]"i"(offsetof(struct kvm_vcpu, regs[VCPU_REGS_R13])),
+		[r14]"i"(offsetof(struct kvm_vcpu, regs[VCPU_REGS_R14])),
+		[r15]"i"(offsetof(struct kvm_vcpu, regs[VCPU_REGS_R15])),
+#endif
+		[cr2]"i"(offsetof(struct kvm_vcpu, regs[VCPU_REGS_CR2]))
 	      : "cc", "memory" );
 
 	++kvm_stat.exits;
