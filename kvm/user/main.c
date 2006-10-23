0cd3003913f301d88b6800f963c8bb136068e7c3
Avi Kivity <avi@qumranet.com>
2006-10-23 10:48:50 +0000

    kvm: test userspace: start flatfile at 1M
    in order to avoid the vga hole at 640K

--- kvm/user/main.c
+++ kvm/user/main.c
@@ -124,7 +124,7 @@ int main(int ac, char **av)
 	if (ac > 1)
 	    load_file(vm_mem + 0xf0000, av[1]);
 	if (ac > 2)
-	    load_file(vm_mem, av[2]);
+	    load_file(vm_mem + 0x100000, av[2]);
 	kvm_show_regs(kvm, 0);
 
 	kvm_run(kvm, 0);
