92985d29ffbad0d98555dfe74696448fdb7db6e5
Uri Lublin <uril@qumranet.com>
2006-11-02 11:56:39 +0000

    kvm debug: apply kernel coding style
    We keep kernel coding style for kvmctl.c too; replaced spaces with a tab.

--- kvm/user/kvmctl.c
+++ kvm/user/kvmctl.c
@@ -499,5 +499,5 @@ int kvm_guest_debug(kvm_context_t kvm, int vcpu, struct kvm_debug_guest *dbg)
 
 int kvm_dump_vcpu(kvm_context_t kvm, int vcpu)
 {
-    return ioctl(kvm->fd, KVM_DUMP_VCPU, vcpu);
+	return ioctl(kvm->fd, KVM_DUMP_VCPU, vcpu);
 }
