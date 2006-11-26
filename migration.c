6200897b6c149c96808d612213b43fb61114ee48
Uri Lublin <uril@qumranet.com>
2006-11-26 17:26:20 +0000

    qemu: migration: will support only full system (ifndef CONFIG_USER_ONLY)
    The bottom of the file (where CONFIG_USER_ONLY is defined) will not change

--- migration.c
+++ migration.c
@@ -2,6 +2,7 @@
 
 #define TO_BE_IMPLEMENTED term_printf("%s: TO_BE_IMPLEMENTED\n", __FUNCTION__)
 
+#ifndef CONFIG_USER_ONLY
 void do_migration_listen(char *arg1, char *arg2){ TO_BE_IMPLEMENTED; }
 void do_migration_connect(char *arg1, char *arg2){ TO_BE_IMPLEMENTED; }
 void do_migration_getfd(int fd) { TO_BE_IMPLEMENTED; }
@@ -10,3 +11,16 @@ void do_migration_cancel(void){ TO_BE_IMPLEMENTED; }
 void do_migration_status(void){ TO_BE_IMPLEMENTED; }
 void do_migration_set(char *fmt, ...){ TO_BE_IMPLEMENTED; }
 void do_migration_show(void){ TO_BE_IMPLEMENTED; }
+
+#else /* CONFIG_USER_ONLY is defined */
+
+void do_migration_listen(char *arg1, char *arg2) { TO_BE_IMPLEMENTED; }
+void do_migration_connect(char *arg1, char *arg2) { TO_BE_IMPLEMENTED; }
+void do_migration_getfd(int fd) { TO_BE_IMPLEMENTED; }
+void do_migration_start(char *deadoralive) { TO_BE_IMPLEMENTED; }
+void do_migration_cancel(void){ TO_BE_IMPLEMENTED; }
+void do_migration_status(void){ TO_BE_IMPLEMENTED; }
+void do_migration_set(char *fmt, ...){ TO_BE_IMPLEMENTED; }
+void do_migration_show(void){ TO_BE_IMPLEMENTED; }
+
+#endif /* of CONFIG_USER_ONLY is defined */
