89bdc237c6ccd71ee25995691ba3b44ea9459990
Avi Kivity <avi@qumranet.com>
2007-01-02 16:19:22 +0000

    kvm: support building an external module against a kernel than includes kvm

---
+++ kvm/kernel/external-module-compat.h
@@ -0,0 +1,12 @@
+
+/*
+ * Compatibility header for building as an external module.
+ */
+
+/*
+ * Avoid picking up the kernel's kvm.h in case we have a newer one.
+ */
+
+#include <linux/compiler.h>
+#include "include/linux/kvm.h"
+
