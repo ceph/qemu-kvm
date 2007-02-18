0362b809aab7c1ee2eec12382e2bc9f7f1aba472
Avi Kivity <avi@qumranet.com>
2007-02-18 09:40:54 +0000

    kvm: libkvm: print out more registers on error

--- kvm/user/kvmctl.c
+++ kvm/user/kvmctl.c
@@ -447,10 +447,26 @@ int kvm_set_msrs(kvm_context_t kvm, int vcpu, struct kvm_msr_entry *msrs,
     return r;
 }
 
+static void print_seg(FILE *file, const char *name, struct kvm_segment *seg)
+{
+    	fprintf(stderr,
+		"%s %04x (%08x/%08x p %d dpl %d db %d s %d type %x l %d"
+		" g %d avl %d)\n",
+		name, seg->selector, seg->base, seg->limit, seg->present,
+		seg->dpl, seg->db, seg->s, seg->type, seg->l, seg->g,
+		seg->avl);
+}
+
+static void print_dt(FILE *file, const char *name, struct kvm_dtable *dt)
+{
+    	fprintf(stderr, "%s %llx/%x\n", name, dt->base, dt->limit);
+}
+
 void kvm_show_regs(kvm_context_t kvm, int vcpu)
 {
 	int fd = kvm->fd;
 	struct kvm_regs regs;
+	struct kvm_sregs sregs;
 	int r;
 
 	regs.vcpu = vcpu;
@@ -470,6 +486,26 @@ void kvm_show_regs(kvm_context_t kvm, int vcpu)
 		regs.r8,  regs.r9,  regs.r10, regs.r11,
 		regs.r12, regs.r13, regs.r14, regs.r15,
 		regs.rip, regs.rflags);
+	sregs.vcpu = vcpu;
+	r = ioctl(fd, KVM_GET_SREGS, &sregs);
+	if (r == -1) {
+		perror("KVM_GET_SREGS");
+		return;
+	}
+	print_seg(stderr, "cs", &sregs.cs);
+	print_seg(stderr, "ds", &sregs.ds);
+	print_seg(stderr, "es", &sregs.es);
+	print_seg(stderr, "ss", &sregs.ss);
+	print_seg(stderr, "fs", &sregs.fs);
+	print_seg(stderr, "gs", &sregs.gs);
+	print_seg(stderr, "tr", &sregs.tr);
+	print_seg(stderr, "ldt", &sregs.ldt);
+	print_dt(stderr, "gdt", &sregs.gdt);
+	print_dt(stderr, "idt", &sregs.idt);
+	fprintf(stderr, "cr0 %llx cr2 %llx cr3 %llx cr4 %llx cr8 %llx"
+		" efer %llx\n",
+		sregs.cr0, sregs.cr2, sregs.cr3, sregs.cr4, sregs.cr8,
+		sregs.efer);
 }
 
 static int handle_cpuid(kvm_context_t kvm, struct kvm_run *run)
@@ -605,6 +641,7 @@ again:
 			fprintf(stderr, "exception %d (%x)\n", 
 			       kvm_run.ex.exception,
 			       kvm_run.ex.error_code);
+			kvm_show_regs(kvm, vcpu);
 			abort();
 			break;
 		case KVM_EXIT_IO:
