7ba8af35faa0eda59a7e91e5085ba8d526609796
Avi Kivity <avi@qumranet.com>
2007-03-26 09:36:47 +0000

    Restore kvm-17 code to use the host's cpuid vendor string
    From: Sergey Kiselev <sergey.kiselev@intel.com>
    
    This re-fixes the compatibility mode guest syscall failure.  The code was
    dropped by the userspace ABI merge.
    
    Signed-off-by: Sergey Kiselev <sergey.kiselev@intel.com>

--- qemu-kvm.c
+++ qemu-kvm.c
@@ -693,6 +693,37 @@ static void do_cpuid_ent(struct kvm_cpuid_entry *e, uint32_t function)
 	if ((h_edx & 0x00100000) == 0)
 	    e->edx &= ~0x00100000u;
     }
+    // sysenter isn't supported on compatibility mode on AMD.  and syscall
+    // isn't supported in compatibility mode on Intel.  so advertise the
+    // actuall cpu, and say goodbye to migration between different vendors
+    // is you use compatibility mode.
+    if (function == 0) {
+	uint32_t bcd[3];
+	asm (
+#ifdef __x86_64__
+	     "push %%rax; push %%rbx; push %%rcx; push %%rdx \n\t"
+	     "mov $0, %%eax \n\t"
+	     "cpuid \n\t"
+	     "mov (%%rsp), %%rax \n\t"
+	     "mov %%ebx, (%%rax) \n\t"
+	     "mov %%ecx, 4(%%rax) \n\t"
+	     "mov %%edx, 8(%%rax) \n\t"
+	     "pop %%rdx; pop %%rcx; pop %%rbx; pop %%rax"
+#else
+	     "push %%eax; push %%ebx; push %%ecx; push %%edx \n\t"
+	     "mov $0, %%eax \n\t"
+	     "cpuid \n\t"
+	     "mov (%%esp), %%eax \n\t"
+	     "mov %%ebx, (%%eax) \n\t"
+	     "mov %%ecx, 4(%%eax) \n\t"
+	     "mov %%edx, 8(%%eax) \n\t"
+	     "pop %%edx; pop %%ecx; pop %%ebx; pop %%eax"
+#endif
+	     : : "d"(bcd) : "memory");
+	e->ebx = bcd[0];
+	e->ecx = bcd[1];
+	e->edx = bcd[2];
+    }
 }
 
 int kvm_qemu_init_env(CPUState *cenv)
