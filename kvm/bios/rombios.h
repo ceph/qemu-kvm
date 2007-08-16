bcd58d0e6d657f9063204be350e7a3866f51e69e
Avi Kivity <avi@qumranet.com>
2007-08-16 10:46:59 +0300

    kvm: bios: apply qemu's original pc-bios/bios.diff

--- kvm/bios/rombios.h
+++ kvm/bios/rombios.h
@@ -19,7 +19,7 @@
 //  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
 /* define it to include QEMU specific code */
-//#define BX_QEMU
+#define BX_QEMU
 
 #ifndef LEGACY
 #  define BX_ROMBIOS32     1
