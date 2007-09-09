4472c764720757b1801c62e145e33fafcaf56c89
Avi Kivity <avi@qumranet.com>
2007-09-09 10:58:04 +0300

    Clear HALT mask for AP when using kernel irqchip
    AP HALT mask is used for userspace irqchip, when kernel irqchip is used,
    this flag will cause AP to block forever.
    
    Signed-off-by: Avi Kivity <avi@qumranet.com>
    Signed-off-by: Qing He <qing.he@intel.com>

--- qemu-kvm.c
+++ qemu-kvm.c
@@ -770,6 +770,8 @@ static void *ap_main_loop(void *_env)
     sigprocmask(SIG_BLOCK, &signals, NULL);
     kvm_create_vcpu(kvm_context, env->cpu_index);
     kvm_qemu_init_env(env);
+    if (kvm_irqchip_in_kernel(kvm_context))
+	env->hflags &= ~HF_HALTED_MASK;
     kvm_main_loop_cpu(env);
     return NULL;
 }
