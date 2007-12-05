e44886f7c9e5f2c9906649cd6da0294736eecaf8
Zhang Xiantao <xiantao.zhang@intel.com>
2007-12-05 11:37:12 +0800

    Add qemu-kvm-ia64 support
    In this patch, one file qemu-kvm-ia64.c is created to
    hold ia64-specific qemu/kvm interfaces.
    
    Signed-off-by: Zhang Xiantao <xiantao.zhang@intel.com>
    Signed-off-by: Avi Kivity <avi@qumranet.com>

---
+++ qemu-kvm-ia64.c
@@ -0,0 +1,71 @@
+
+#include "config.h"
+#include "config-host.h"
+
+extern int kvm_allowed;
+extern int kvm_irqchip;
+
+#ifdef USE_KVM
+
+#include <string.h>
+#include "vl.h"
+
+#include "qemu-kvm.h"
+#include <libkvm.h>
+#include <pthread.h>
+#include <sys/utsname.h>
+
+
+extern kvm_context_t kvm_context;
+
+extern __thread CPUState *vcpu_env;
+
+int kvm_arch_qemu_create_context(void)
+{
+	return 0;
+}
+
+void kvm_arch_load_regs(CPUState *env)
+{
+}
+
+
+void kvm_arch_save_regs(CPUState *env)
+{
+}
+
+int kvm_arch_qemu_init_env(CPUState *cenv)
+{
+    return 0;
+}
+
+int kvm_arch_halt(void *opaque, int vcpu)
+{
+    CPUState *env = cpu_single_env;
+    env->hflags |= HF_HALTED_MASK;
+    env->exception_index = EXCP_HLT;
+    return 1;
+}
+
+void kvm_arch_pre_kvm_run(void *opaque, int vcpu)
+{
+}
+
+void kvm_arch_post_kvm_run(void *opaque, int vcpu)
+{
+}
+
+int kvm_arch_has_work(CPUState *env)
+{
+	return 1;
+}
+
+int kvm_arch_try_push_interrupts(void *opaque)
+{
+	return 1;
+}
+
+void kvm_arch_update_regs_for_sipi(CPUState *env)
+{
+}
+#endif
