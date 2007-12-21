090ccc0676b394b03efc6fb9ec9fd8bf6eccfdc3
Carlo Marcelo Arenas Belon <carenas@sajinet.com.pe>
2007-12-21 06:39:03 -0600

    Add missing declaration for qemu_system_reset
    The following patch fixes :
    
      qemu/qemu-kvm.c:349: warning: implicit declaration of function `qemu_system_reset'
    
    Signed-off-by: Carlo Marcelo Arenas Belon <carenas@sajinet.com.pe>
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- sysemu.h
+++ sysemu.h
@@ -39,6 +39,7 @@ int qemu_powerdown_requested(void);
 #else
 void qemu_system_powerdown(void);
 #endif
+void qemu_system_reset(void);
 
 void cpu_save(QEMUFile *f, void *opaque);
 int cpu_load(QEMUFile *f, void *opaque, int version_id);
