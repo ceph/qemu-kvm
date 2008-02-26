55ba7ef08c2888a1c8d108f10b084ae5a93a81aa
Jerone Young <jyoung5@us.ibm.com>
2008-02-26 11:18:30 -0600

    Fix PowerPC CPU initilization when using target-ppc/fake-exec.c
    Signed-off-by: Jerone Young <jyoung5@us.ibm.com>
    Acked-by: Hollis Blanchard <hollisb@us.ibm.com>
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- target-ppc/fake-exec.c
+++ target-ppc/fake-exec.c
@@ -20,6 +20,22 @@
 #include "cpu.h"
 #include "exec-all.h"
 
+
+struct ppc_def_t {
+    const unsigned char *name;
+    uint32_t pvr;
+    uint32_t svr;
+    uint64_t insns_flags;
+    uint64_t msr_mask;
+    powerpc_mmu_t   mmu_model;
+    powerpc_excp_t  excp_model;
+    powerpc_input_t bus_model;
+    uint32_t flags;
+    int bfd_mach;
+    void (*init_proc)(CPUPPCState *env);
+    int  (*check_pow)(CPUPPCState *env);
+};
+
 int code_copy_enabled = 0;
 
 void cpu_dump_state (CPUState *env, FILE *f,
@@ -60,13 +76,26 @@ int cpu_ppc_gen_code(CPUState *env, TranslationBlock *tb, int *gen_code_size_ptr
     return 0;
 }
 
+void init_proc_ppc440ep_kvm(CPUPPCState *env)
+{
+    ppc40x_irq_init(env);
+}
+
+static ppc_def_t ppc440ep_kvm = {
+    .name = "440EP KVM",
+    .mmu_model = POWERPC_MMU_SOFT_4xx, /*XXX needed for GDB stub */
+    .init_proc = init_proc_ppc440ep_kvm,
+};
+
 const ppc_def_t *cpu_ppc_find_by_name (const unsigned char *name)
 {
-    return NULL;
+    return &ppc440ep_kvm;
 }
 
 int cpu_ppc_register_internal (CPUPPCState *env, const ppc_def_t *def)
 {
+    env->mmu_model = def->mmu_model;
+    (*def->init_proc)(env);
     return 0;
 }
 
