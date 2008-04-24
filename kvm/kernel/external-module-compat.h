5bad1b71c33911e4b7b39db1ef5c3fc4cdc8feb3
Avi Kivity <avi@qumranet.com>
2008-04-24 12:01:56 +0300

    kvm: external module: move smp_call_function_single() compat into .c file
    This gives better traces when something goes wrong.
    
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- kvm/kernel/external-module-compat.h
+++ kvm/kernel/external-module-compat.h
@@ -39,76 +39,15 @@
 #endif
 
 /*
- * smp_call_function_single() is not exported below 2.6.20
+ * smp_call_function_single() is not exported below 2.6.20, and has different
+ * semantics below 2.6.23.
  */
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,20)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,23)
 
-#include <linux/spinlock.h>
-#include <linux/smp.h>
+int kvm_smp_call_function_single(int cpu, void (*func)(void *info),
+				 void *info, int nonatomic, int wait);
 
-struct scfs_thunk_info {
-	int cpu;
-	void (*func)(void *info);
-	void *info;
-};
-
-static inline void scfs_thunk(void *_thunk)
-{
-	struct scfs_thunk_info *thunk = _thunk;
-
-	if (raw_smp_processor_id() == thunk->cpu)
-		thunk->func(thunk->info);
-}
-
-static inline int smp_call_function_single1(int cpu, void (*func)(void *info),
-					   void *info, int nonatomic, int wait)
-{
-	int r, this_cpu;
-	struct scfs_thunk_info thunk;
-
-	this_cpu = get_cpu();
-	if (cpu == this_cpu) {
-		r = 0;
-		local_irq_disable();
-		func(info);
-		local_irq_enable();
-	} else {
-		thunk.cpu = cpu;
-		thunk.func = func;
-		thunk.info = info;
-		r = smp_call_function(scfs_thunk, &thunk, 0, 1);
-	}
-	put_cpu();
-	return r;
-}
-
-#define smp_call_function_single smp_call_function_single1
-
-#elif LINUX_VERSION_CODE < KERNEL_VERSION(2,6,23)
-/*
- * pre 2.6.23 doesn't handle smp_call_function_single on current cpu
- */
-
-#include <linux/smp.h>
-
-static inline int smp_call_function_single2(int cpu, void (*func)(void *info),
-					    void *info, int nonatomic, int wait)
-{
-	int this_cpu, r;
-
-	this_cpu = get_cpu();
-	if (cpu == this_cpu) {
-		r = 0;
-		local_irq_disable();
-		func(info);
-		local_irq_enable();
-	} else
-		r = smp_call_function_single(cpu, func, info, nonatomic, wait);
-	put_cpu();
-	return r;
-}
-
-#define smp_call_function_single smp_call_function_single2
+#define smp_call_function_single kvm_smp_call_function_single
 
 #endif
 
