c40ccca650d6d67ed50e1f4dd261e8f901bf469c
Anthony Liguori <aliguori@us.ibm.com>
2008-05-07 16:40:58 -0500

    Fix e1000 can_receive handler
    The current logic of the can_receive handler is to allow packets whenever the
    receiver is disabled or when there are descriptors available in the ring.
    
    I think the logic ought to be to allow packets whenever the receiver is enabled
    and there are descriptors available in the ring.
    
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>
    Signed-off-by: Avi Kivity <avi@qumranet.com>

--- hw/e1000.c
+++ hw/e1000.c
@@ -520,8 +520,8 @@ e1000_can_receive(void *opaque)
 {
     E1000State *s = opaque;
 
-    return (!(s->mac_reg[RCTL] & E1000_RCTL_EN) ||
-            s->mac_reg[RDH] != s->mac_reg[RDT]);
+    return ((s->mac_reg[RCTL] & E1000_RCTL_EN) &&
+	    s->mac_reg[RDH] != s->mac_reg[RDT]);
 }
 
 static void
