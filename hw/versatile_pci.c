195a3ceaabcabba748c5dc002755e7b433d9f510
Amit Shah <amit.shah@qumranet.com>
2008-05-26 15:19:20 +0530

    QEMU: Fill in PCI subsystem vendor id for versatile_pci
    The subsystem vendor ID shouldn't be 0x0 or 0xffff according
    to the PCI spec
    
    Signed-off-by: Amit Shah <amit.shah@qumranet.com>

--- hw/versatile_pci.c
+++ hw/versatile_pci.c
@@ -124,8 +124,8 @@ PCIBus *pci_vpb_init(qemu_irq *pic, int irq, int realview)
         isa_mmio_init(base + 0x03000000, 0x00100000);
     }
 
-    d->config[0x00] = 0xee; // vendor_id
-    d->config[0x01] = 0x10;
+    d->config[0x00] = d->config[0x2c] = 0xee; // vendor_id
+    d->config[0x01] = d->config[0x2d] = 0x10;
     /* Both boards have the same device ID.  Oh well.  */
     d->config[0x02] = 0x00; // device_id
     d->config[0x03] = 0x03;
