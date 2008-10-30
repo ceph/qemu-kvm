020e1c16434162fc3901da47369aec5bb04b286f
Avi Kivity <avi@redhat.com>
2008-10-30 12:58:12 +0200

    kvm: external module: dynamic unifdef
    The current unifdef solution mangles the source files to fit just one
    architecture; this causes problem shipping one source tarball for many
    consumers on different architectures.
    
    Change to a dynamic model.  Instead of removing ifdefs, figure out which
    symbols are defined on which architecture, and define them appropriately.
    
    Signed-off-by: Avi Kivity <avi@redhat.com>

---
+++ kvm/kernel/unifdef.h
@@ -0,0 +1,28 @@
+#ifndef KVM_UNIFDEF_H
+#define KVM_UNIFDEF_H
+
+#ifdef __i386__
+#ifndef CONFIG_X86_32
+#define CONFIG_X86_32 1
+#endif
+#endif
+
+#ifdef __x86_64__
+#ifndef CONFIG_X86_64
+#define CONFIG_X86_64 1
+#endif
+#endif
+
+#if defined(__i386__) || defined (__x86_64__)
+#ifndef CONFIG_X86
+#define CONFIG_X86 1
+#endif
+#endif
+
+#ifdef __ia64__
+#ifndef CONFIG_IA64
+#define CONFIG_IA64 1
+#endif
+#endif
+
+#endif
