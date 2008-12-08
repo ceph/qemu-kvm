c5769dabe28332d81c2ad4a81b0ead8f52a0d194
Amit Shah <amit.shah@redhat.com>
2008-12-08 14:39:53 +0000

    kvm: testsuite: Add VMMCALL hypercall test
    By default, hypercalls issue the VMCALL instruction, which is only supported
    on Intel. On AMD, we detect this and patch the call site with VMMCALL, which
    is only supported on AMD. When the code issues VMMCALL again, the hypercall
    is called.
    
    This works fine on both Intel as well as AMD.
    
    When live-migrating guests from AMD machines to Intel machines, the guest
    issues a VMMCALL, which should then be patched to VMCALL. Add a test for
    this behaviour.
    
    Signed-off-by: Amit Shah <amit.shah@redhat.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- kvm/user/test/x86/hypercall.c
+++ kvm/user/test/x86/hypercall.c
@@ -1,11 +1,21 @@
 #include "libcflat.h"
 
-#define KVM_HYPERCALL ".byte 0x0f,0x01,0xc1"
+#define KVM_HYPERCALL_INTEL ".byte 0x0f,0x01,0xc1"
+#define KVM_HYPERCALL_AMD ".byte 0x0f,0x01,0xd9"
 
-static inline long kvm_hypercall0(unsigned int nr)
+static inline long kvm_hypercall0_intel(unsigned int nr)
 {
 	long ret;
-	asm volatile(KVM_HYPERCALL
+	asm volatile(KVM_HYPERCALL_INTEL
+		     : "=a"(ret)
+		     : "a"(nr));
+	return ret;
+}
+
+static inline long kvm_hypercall0_amd(unsigned int nr)
+{
+	long ret;
+	asm volatile(KVM_HYPERCALL_AMD
 		     : "=a"(ret)
 		     : "a"(nr));
 	return ret;
@@ -13,7 +23,9 @@ static inline long kvm_hypercall0(unsigned int nr)
 
 int main(int ac, char **av)
 {
-	kvm_hypercall0(-1u);
-	printf("Hypercall: OK\n");
+	kvm_hypercall0_intel(-1u);
+	printf("Hypercall via VMCALL: OK\n");
+	kvm_hypercall0_amd(-1u);
+	printf("Hypercall via VMMCALL: OK\n");
 	return 0;
 }
