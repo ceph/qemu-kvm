25b05de0d9aad5cf41574afd55f6990f31fa7561
Anthony Liguori <aliguori@us.ibm.com>
2009-04-29 16:20:27 -0500

    Remove IBM copyright in unmodified file in upstream
    Presumably, it would carry an IBM copyright upstream if needed.  qemu-kvm
    introduces no additional code over upstream QEMU in this file.
    
    Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>
    Acked-by: Hollis Blanchard <hollisb@us.ibm.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- hw/ppc4xx.h
+++ hw/ppc4xx.h
@@ -3,9 +3,6 @@
  *
  * Copyright (c) 2007 Jocelyn Mayer
  *
- * Copyright 2008 IBM Corp.
- * Authors: Hollis Blanchard <hollisb@us.ibm.com>
- *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
