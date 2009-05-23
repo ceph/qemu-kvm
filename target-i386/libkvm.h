d1ce2f5c6e628f2134763366fcc2f6d5cdce4d31
Jan Kiszka <jan.kiszka@web.de>
2009-05-23 21:25:01 +0200

    libkvm: Drop redundant tpr prototypes
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>

--- target-i386/libkvm.h
+++ target-i386/libkvm.h
@@ -25,31 +25,6 @@
 
 int kvm_set_tss_addr(kvm_context_t kvm, unsigned long addr);
 
-#ifdef KVM_CAP_VAPIC
-
-/*!
- * \brief Enable kernel tpr access reporting
- *
- * When tpr access reporting is enabled, the kernel will call the
- * ->tpr_access() callback every time the guest vcpu accesses the tpr.
- *
- * \param kvm Pointer to the current kvm_context
- * \param vcpu vcpu to enable tpr access reporting on
- */
-int kvm_enable_tpr_access_reporting(kvm_context_t kvm, int vcpu);
-
-/*!
- * \brief Disable kernel tpr access reporting
- *
- * Undoes the effect of kvm_enable_tpr_access_reporting().
- *
- * \param kvm Pointer to the current kvm_context
- * \param vcpu vcpu to disable tpr access reporting on
- */
-int kvm_disable_tpr_access_reporting(kvm_context_t kvm, int vcpu);
-
-#endif
-
 #define smp_wmb()   asm volatile("" ::: "memory")
 
 #endif
