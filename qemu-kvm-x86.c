fd6bfef3d49a62b36092f131690de3e34f84662d
Bernhard Kohl <bernhard.kohl@nsn.com>
2010-09-14 15:51:40 +0200

    qemu-kvm-x86: consider the irq0override flag in kvm_arch_init_irq_routing
    The setting of the irq0override flag must be also passed properly
    to the KVM_IRQCHIP_IOAPIC.
    
    Signed-off-by: Bernhard Kohl <bernhard.kohl@nsn.com>
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>

--- qemu-kvm-x86.c
+++ qemu-kvm-x86.c
@@ -1388,9 +1388,9 @@ int kvm_arch_init_irq_routing(void)
             }
         }
         for (i = 0; i < 24; ++i) {
-            if (i == 0) {
+            if (i == 0 && irq0override) {
                 r = kvm_add_irq_route(kvm_context, i, KVM_IRQCHIP_IOAPIC, 2);
-            } else if (i != 2) {
+            } else if (i != 2 || !irq0override) {
                 r = kvm_add_irq_route(kvm_context, i, KVM_IRQCHIP_IOAPIC, i);
             }
             if (r < 0) {
